15ac1194aa933083619dfdfdee70c74d0b4d7a62
mrsomebody02 <iliecristian12@gmail.com>
2023-02-05 16:49:57 +0200

    final final

--- script.js
+++ script.js
@@ -2,17 +2,11 @@
 function submit()
 {
     let v = document.querySelector('input[name="btn"]:checked').value;
-    console.log(v);
-    if(v&&null)
-    {
-
-    }
-    else
+    if(v&&!null)
     {
         document.getElementById('submitContainer').style.display="none";
         document.getElementById('rateContainer').style.display="flex";
         document.getElementById('rateLvl').textContent="You selected " + v + " out of 5";
     }
-    
-    
+   
 }
